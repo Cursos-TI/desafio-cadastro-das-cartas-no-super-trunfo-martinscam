1be040350ab4484a4705f60169dfc8c9b6680e84
martinscam <santscamila812@gmail.com>
2025-03-16 12:26:42 +0000

    Retirei excessos de espaços que pudessem estar bagunçando o código. Otimizei os textos para ficarem mais claros, principalmente os comentários. Alterei o local de exibição da informação de entrada. Agora, em vez de digitar a informação de entrada embaixo da frase, a pessoa digita a informação na frente da frase. Organização dos cálculos no código.

--- super_trunfo.c
+++ super_trunfo.c
@@ -18,75 +18,71 @@ int main(){
     float densidade2, percapita2;
     float superpoder2;
 
-      
     //Apresentação dos comandos
 
-    printf("Realize o cadastro de duas cartas de Super Trunfo, seguindo as próximas instruções: \n");
-
-
-    printf("\n"); // Adiciona uma linha em branco
-
-
-    //Solicitação e coleta dos dados da carta 1
-
-
-    printf("Defina uma letra entre A e H para representar o primeiro estado: \n");
-    scanf("%c", &estado1); //Solicitação e coleta da letra que representa o estado 1
-
-    printf("Defina o código da primeira carta, digitando a letra do primeiro estado, seguida de um número de 01 a 04 (exemplo: A01, A02...): \n");
-    scanf("%s", &codigocarta1); //Solicitação e coleta do código da carta 1
-
-    printf("Digite o nome da cidade: \n");
-    scanf("%s", &cidade1); //Solicitação e coleta do nome da cidade 1
-
-    printf("Defina o número de habitantes da cidade: \n");
-    scanf("%d", &populacao1); //Solicitação e coleta da população da cidade 1
-
-    printf("Digite a área da cidade em quilômetros quadrados: \n");
-    scanf("%f", &area1); //Solicitação e coleta da área da cidade 1 em km² 
-
-    printf("Digite o PIB - Produto Interno Bruto da cidade: \n");
-    scanf("%f", &pib1); //Solicitação e coleta do PIB da cidade 1
-
-    printf("Digite o número de pontos turísticos existentes na cidade: \n");
-    scanf("%d", &pontosturisticos1); //Solicitação do número de pontos turísticos existentes na cidade 1
-
+    printf("Realize o cadastro de duas cartas de Super Trunfo, seguindo as próximas instruções:\n");
 
     printf("\n"); // Adiciona uma linha em branco
 
+    //Solicitação e entrada dos dados da carta 1
+
+    printf("Defina uma letra entre A e H para representar o primeiro estado: ");
+    scanf("%c", &estado1); //Solicitação e entrada da letra que representa o estado 1
+
+    printf("Defina o código da primeira carta, digitando a letra do primeiro estado, seguida de um número de 01 a 04 (exemplo: A01, A02...): ");
+    scanf("%s", &codigocarta1); //Solicitação e entrada do código da carta 1
+
+    printf("Digite o nome da cidade: ");
+    scanf("%s", &cidade1); //Solicitação e entrada do nome da cidade 1
+
+    printf("Defina o número de habitantes da cidade: ");
+    scanf("%d", &populacao1); //Solicitação e entrada da população da cidade 1
+
+    printf("Digite a área da cidade em quilômetros quadrados: ");
+    scanf("%f", &area1); //Solicitação e entrada da área da cidade 1 em km² 
+
+    printf("Digite o PIB - Produto Interno Bruto da cidade: ");
+    scanf("%f", &pib1); //Solicitação e entrada do PIB da cidade 1
+
+    printf("Digite o número de pontos turísticos existentes na cidade: ");
+    scanf("%d", &pontosturisticos1); //Solicitação e entrada do número de pontos turísticos existentes na cidade 1
+
+    printf("\n"); // Adiciona uma linha em branco
 
     //Solicitação e coleta dos dados da carta 2
 
-
-    printf("Defina uma letra entre A e H para representar o segundo estado: \n");
-    scanf(" %c", &estado2); //Solicitação e coleta da letra que representa o estado 2
+    printf("Defina uma letra entre A e H para representar o segundo estado: ");
+    scanf(" %c", &estado2); //Solicitação e entrada da letra que representa o estado 2
     
-    printf("Defina o código da carta, digitando a letra do segundo estado, seguida de um número de 01 a 04 (exemplo: A01, A02...): \n");
-    scanf("%s", &codigocarta2); //Solicitação e coleta do nome do estado 2
+    printf("Defina o código da carta, digitando a letra do segundo estado, seguida de um número de 01 a 04 (exemplo: A01, A02...): ");
+    scanf("%s", &codigocarta2); //Solicitação e entrada do nome do estado 2
     
-    printf("Digite o nome da cidade: \n");
-    scanf("%s", &cidade2); //Solicitação e coleta do nome da cidade 2
+    printf("Digite o nome da cidade: ");
+    scanf("%s", &cidade2); //Solicitação e entrada do nome da cidade 2
     
-    printf("Defina o número de habitantes da cidade: \n");
-    scanf("%d", &populacao2); //Solicitação e coleta da população da cidade 2
+    printf("Defina o número de habitantes da cidade: ");
+    scanf("%d", &populacao2); //Solicitação e entrada da população da cidade 2
     
-    printf("Digite a área da cidade em quilômetros quadrados: \n");
-    scanf("%f", &area2); //Solicitação e coleta da área da cidade 2 em km² 
+    printf("Digite a área da cidade em quilômetros quadrados: ");
+    scanf("%f", &area2); //Solicitação e entrada da área da cidade 2 em km² 
     
-    printf("Digite o PIB - Produto Interno Bruto da cidade: \n");
-    scanf("%f", &pib2); //Solicitação e coleta do PIB da cidade 2
+    printf("Digite o PIB - Produto Interno Bruto da cidade: ");
+    scanf("%f", &pib2); //Solicitação e entrada do PIB da cidade 2
     
-    printf("Digite o número de pontos turísticos existentes na cidade: \n");
-    scanf("%d", &pontosturisticos2); //Solicitação do número de pontos turísticos existentes na cidade 2
-
+    printf("Digite o número de pontos turísticos existentes na cidade: ");
+    scanf("%d", &pontosturisticos2); //Solicitação e entrada do número de pontos turísticos existentes na cidade 2
 
     printf("\n"); // Adiciona uma linha em branco
 
+    //Calculos da carta 1
 
+    densidade1 = populacao1 / area1; //Calcula a densidade populacional, dividindo o valor populacional pela área da cidade
+    percapita1 = pib1 / populacao1; //Calcula o PIB Per Capita, dividindo o valor do PIB pelo valor populacional da cidade
+    superpoder1 = populacao1 + area1 + pib1 + pontosturisticos1 + percapita1 + (1 / densidade1); //Soma todos os atributos da carta 1, incluindo o inverso da densidade para garantir maior pontuação à carta com menor densidade
+ 
     //Exibição das informações coletadas da carta 1
 
-
-    printf("INFORMAÇÕES DA CARTA 1: \n");
+    printf("INFORMAÇÕES DA CARTA 1:\n");
 
     printf("\n"); // Adiciona uma linha em branco
     
@@ -103,28 +99,24 @@ int main(){
     printf("PIB: %.2f bilhões de reais\n", pib1); //Exibição do PIB da cidade 1
 
     printf("Pontos Turísticos: %d\n", pontosturisticos1); //Exibição do número de pontos turísticos na cidade 1
-
     
-    densidade1 = populacao1 / area1; //Calcula a densidade populacional, dividindo o valor populacional pela área da cidade
-
-    percapita1 = pib1 / populacao1; //Calcula o PIB Per Capita, dividindo o valor do PIB pelo valor populacional da cidade
-
-
-    printf("Densidade Populacional: %.2f hab/km²\n", densidade1); //Exibe o valor da densidade populacional calculado acima
+    printf("Densidade Populacional: %.2f hab/km²\n", densidade1); //Exibe o valor da densidade populacional já calculada
     
-    printf("PIB Per Capita: %.2f reais\n", percapita1); //Exibe o valor do PIB Per Capita calculado acima
-
-
-    superpoder1 = populacao1 + area1 + pib1 + pontosturisticos1 + percapita1 + (1 / densidade1); //Soma todos os atributos da carta 1, incluindo o inverso da densidade para garantir mais pontos para quem tiver a densidade menor
-
-    
+    printf("PIB Per Capita: %.2f reais\n", percapita1); //Exibe o valor do PIB Per Capita já calculado
+   
     printf("\n"); // Adiciona uma linha em branco
 
+    //Calculos da carta 2
+
+    densidade2 = (float) populacao2 / area2; //Calcula a densidade populacional, dividindo o valor populacional pela área da cidade
+
+    percapita2 = (float) pib2 / populacao2; //Calcula o PIB Per Capita, dividindo o valor do PIB pelo valor populacional da cidade
+    
+    superpoder2 = populacao2 + area2 + pib2 + pontosturisticos2 + percapita2 + (1 / densidade2); //Soma todos os atributos da carta 2, incluindo o inverso da densidade para garantir maior pontuação à carta com menor densidade
 
     //Exibição das informações coletadas da carta 2
-
     
-    printf("INFORMAÇÕES DA CARTA 2: \n");
+    printf("INFORMAÇÕES DA CARTA 2:\n");
 
     printf("\n"); // Adiciona uma linha em branco
     
@@ -141,27 +133,17 @@ int main(){
     printf("PIB: %.2f bilhões de reais\n", pib2); //Exibição do PIB da cidade 2
 
     printf("Pontos Turísticos: %d\n", pontosturisticos2); //Exibição do número de pontos turísticos na cidade 2
-
-
-    densidade2 = (float) populacao2 / area2; //Calcula a densidade populacional, dividindo o valor populacional pela área da cidade
-
-    percapita2 = (float) pib2 / populacao2; //Calcula o PIB Per Capita, dividindo o valor do PIB pelo valor populacional da cidade
-
-
+   
     printf("Densidade Populacional: %.2f hab/km²\n", densidade2); //Exibe o valor da densidade populacional calculado acima
     
     printf("PIB Per Capita: %.2f reais\n", percapita2); //Exibe o valor do PIB Per Capita calculado acima
 
-
-    superpoder2 = populacao2 + area2 + pib2 + pontosturisticos2 + percapita2 + (1 / densidade2); //Soma todos os atributos da carta 2, incluindo o inverso da densidade para garantir mais pontos para quem tiver a densidade menor
-
+    printf("\n"); // Adiciona uma linha em branco   
 
     printf("Comparação de Cartas:\n");
 
-
     printf("\n"); // Adiciona uma linha em branco
-
-       
+    
     printf("População: Carta %d venceu (%d)\n", (populacao1 > populacao2) ? 1 : 2, (populacao1 > populacao2) ? 1 : 0); //Utilizado operador ternário para determminar qual carta vence, e retornar o valor final, se verdadeiro ou falso, da forma mais simplificada possível
 
     printf("Área: Carta %d venceu (%d)\n", (area1 > area2) ? 1 : 2, (area1 > area2) ? 1 : 0);
@@ -175,7 +157,6 @@ int main(){
     printf("PIB Per Capita: Carta %d venceu (%d)\n", (percapita1 > percapita2) ? 1 : 2, (percapita1 > percapita2) ? 1 : 0);
 
     printf("Super Poder: Carta %d venceu (%d)\n", (superpoder1 > superpoder2) ? 1 : 2, (superpoder1 > superpoder2) ? 1 : 0);
-
 
 return 0;
 }
